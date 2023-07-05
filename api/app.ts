7aea3836c35539c91bef6665c106afe68c08ca53
Sacramentix <benji.u1225u@gmail.com>
2023-07-05 19:14:14 +0200

    add cors

--- api/app.ts
+++ api/app.ts
@@ -1,10 +1,13 @@
 import { Hono } from "https://deno.land/x/hono@v3.1.8/mod.ts";
+import { cors } from 'https://deno.land/x/hono@v3.1.8/middleware.ts';
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 import { getAccountOrCreate } from "./surreal/query/getAccountOrCreate.ts";
 import { achieve } from "./surreal/query/achieve.ts";
 import { guardOrySession } from "./guard/orySession.ts";
 
 const app = new Hono();
+
+app.use("/*", cors());
 
 const route =
 app
--- api/surreal/query/getAccountOrCreate.ts
+++ api/surreal/query/getAccountOrCreate.ts
@@ -3,16 +3,19 @@ import { db } from "../db.ts";
 
 export async function getAccountOrCreate(user:OryUser) {
     const queryResult = await db.query<[never, SurrealAccount]>(/* surrealql */`
-        LET $account = (SELECT * FROM user:⟨$id⟩)[0];
+        LET $account = (SELECT * FROM $uid)[0];
         RETURN IF $account = NONE THEN {
-            RETURN CREATE user CONTENT { name: $name, id: id, email: $email };
+            RETURN CREATE user CONTENT { name: $name, id: $id, email: $email };
         }
         ELSE {
-            RETURN $account;
+            RETURN UPDATE $uid CONTENT { name: $name email: $email };
         }
         END;
     `,
-    user
+    {
+        ...user,
+        uid: `user:⟨${user.id}⟩`
+    }
     );
     return queryResult[1].result;
 }
